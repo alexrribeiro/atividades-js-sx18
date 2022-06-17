79902cc88ce20bfc6d92149f6d2f1a41ce00d710
Alex Ramos Ribeiro <alexramosribeiro@gmail.com>
2022-06-17 19:55:56 -0300

    Aula 03

--- assets/scripts/condicionais.js
+++ assets/scripts/condicionais.js
@@ -9,4 +9,19 @@ function verificarCidade() {
 function calcularImposto() {
     const anofab = document.getElementById("txtAno").value;
     const valor = document.getElementById("txtValor").value;
+    const anoatual = new Date().getFullYear();
+    const resultado = document.getElementById("resultado");
+    var frase;
+
+    if(anofab > anoatual - 20) {
+        const imposto = valor * 0.04;
+        const impostoFormatado = imposto.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        frase = "Valor a pagar do imposto é: " + impostoFormatado;
+    }
+    else {
+        frase = "O veículo está isento de pagar IPVA.";
+    }
+
+    resultado.innerHTML = frase;
+    resultado.style.display = "block";
 }
